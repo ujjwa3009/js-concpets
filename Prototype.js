3d319f771e05b99636db5e410b706e0a7fadfe3a
Ujjwal Kumar <Ujjwal3009.iimt@gmail.com>
2025-09-02 03:28:23 +0530

    prototypical inherictmnace

---
+++ Prototype.js
@@ -0,0 +1,7 @@
+/**
+ * Prototypicla inheritnace is mechanism in javascript thioruh twhich javascript objects acccess the propertyu from other 
+ * oibject that is linked thoruygh prototype. Each js object jave prototype linked to another objecxt and it looks up till null for any property
+ * 
+ * We cxan also sahred methods via this mecahniums
+ * 
+ */
