44522203ce75cf3ddfb58f2a58c6480808fe64b9
Ujjwal Kumar <Ujjwal3009.iimt@gmail.com>
2025-09-02 03:20:08 +0530

    this keyword

---
+++ this.js
@@ -0,0 +1,7 @@
+/**
+ * this keyword refers the object that executing current function
+ * 
+ * this  - value depends on how function is called not where it is defined
+ * 
+ * it can be 4 types - default , implicit (object scope) , explict (all, apply , bind) , arrow function - preservews lexical  this
+ */
