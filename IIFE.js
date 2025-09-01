86abb54bbaefb9ea395e7af3249e58775ef8e31c
Ujjwal Kumar <Ujjwal3009.iimt@gmail.com>
2025-09-02 02:27:20 +0530

    IIFE

---
+++ IIFE.js
@@ -0,0 +1,16 @@
+// Stabds for immediately invoked functions .. Which is invked just after declaration . 
+
+(function() {
+    console.log("Hell");
+}());
+
+
+/**
+ * It is useful in
+ * Scope isolation
+ * Private Variable
+ * Module Pattern
+ * 
+ * 
+ * Also jquery uses IIFE to not pollut global space with $
+ */
